93840f67a9005be95c6bbeff58d18ada3435921d
yaehei <yaehei@users.noreply.github.com>
2016-05-21 15:13:52 +0800

    Create blockBaiduAds.js

---
+++ blockBaiduAds.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @id             blockBaiduAds
+// @name           blockBaiduAds
+// @version        1.0
+// @namespace      
+// @author         hugbio
+// @description    移除百度搜索结果中的推广链接
+// @include        http://*baidu.com*
+// @run-at         document-end
+// @require        http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js
+// @updateURL     
+// ==/UserScript==
+
+(function(){
+   document.cookie = "ISSW=1";
+}());
